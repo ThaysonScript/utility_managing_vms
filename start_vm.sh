64ddb2411104f79d324095370e0cca1abeda3ad0
ThaysonScript <thaysonguedes01@gmail.com>
2024-02-26 21:59:29 -0300

    more base

--- start_vm.sh
+++ start_vm.sh
@@ -6,10 +6,9 @@ source ./network_config.sh
 qemu-system-x86_64 \
     -enable-kvm                                                    \
     -m "$RAM_MEMORY"                                               \
-    -smp cpus="$CPUS"                                                   \
+    -smp cpus="$CPUS"                                              \
     -hda "$VM_NAME"                                                \
     -boot d                                                        \
-    -cdrom "$ISO_PATH"                                             \
     -netdev "$NETWORK_CONFIG"                                      \
     -device virtio-net-pci,netdev=net0                             \
     -vga qxl                                                       \
--- tags.sh
+++ tags.sh
@@ -5,6 +5,7 @@
 readonly VM_NAME="debian-12.4.0-amd64-netinst.qcow2"
 readonly DISK_SIZE=20G
 readonly RAM_MEMORY=2G
+readonly CPUS=2
 
 
 readonly ISO_PATH="./isos/debian-12.4.0-amd64-netinst.iso"
